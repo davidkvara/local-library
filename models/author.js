f1571cee2e8d862bdefad8887519d868d336cc84
davidkvara <davekvaratskhelia@gmail.com>
2018-04-16 23:26:00 +0400

    date formatting using moment

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 var mongoose = require("mongoose");
+var moment = require("moment");
 
 var Schema = mongoose.Schema;
 
@@ -9,12 +10,29 @@ var AuthorSchema = new Schema({
   date_of_death: { type: Date }
 });
 
+// Virtual for author's full name
 AuthorSchema.virtual("name").get(function() {
   return this.family_name + ", " + this.first_name;
 });
 
+// Virtual for author's URL
 AuthorSchema.virtual("url").get(function() {
   return "/catalog/author/" + this._id;
 });
 
+// Virtual for author's birth Date
+AuthorSchema.virtual("date_of_birth_formatted").get(function() {
+  return this.date_of_birth
+    ? moment(this.date_of_birth).format("DD/MM/YYYY")
+    : "no data";
+});
+
+// Virtual for author's death Date
+AuthorSchema.virtual("date_of_death_formatted").get(function() {
+  return this.date_of_death
+    ? moment(this.date_of_death).format("DD/MM/YYYY")
+    : "no data";
+});
+
+//Export model
 module.exports = mongoose.model("Author", AuthorSchema);
