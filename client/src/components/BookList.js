9dd622271f739e1a27a7bd2879db5d61162fb0b3
davidkvara <davekvaratskhelia@gmail.com>
2018-05-28 08:44:33 +0400

    replace a with Link

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function BookList(props) {
   return (
     <div>
       {props.books.map(book => (
         <p className="book" key={book.id}>
-          <a href={book.url}>{book.title}</a>-
+          <Link to={book.url}>{book.title}</Link>-
           {book.author.name}
         </p>
       ))}
--- client/src/routes/Authors.js
+++ client/src/routes/Authors.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 class Author extends React.Component {
   state = { author_list: [] };
@@ -16,7 +17,7 @@ class Author extends React.Component {
         <h2 className="page-title">Author List</h2>
         {this.state.author_list.map(author => (
           <p key={author.id}>
-            <a href={author.url}>{author.name}</a>
+            <Link to={author.url}>{author.name}</Link>
             <span className="smaller">
               ({author.date_of_birth_formatted}-{author.date_of_death_formatted})
             </span>
